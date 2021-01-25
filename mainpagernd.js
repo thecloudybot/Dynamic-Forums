abd9e5d266dce563c1347aeccb051d64abd8a14e
WitzIsHot <71299687+WitzIsHot@users.noreply.github.com>
2021-01-24 17:48:26 -0800

    Delete mainpagernd.js

--- mainpagernd.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-
-  var images = ['https://www.lovethispic.com/uploaded_images/20873-Trippy-Gif.gif?1'];
-  document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
-
-</script>
